2574b79484d9439994e5d8caf5ba8bc6d3aed52b
janghyeonwoo <blue672@naver.com>
2021-11-04 22:52:08 +0900

    다중 Step 구성하기

---
+++ src/main/java/com/example/springbatch/config/JobConfig.java
@@ -0,0 +1,67 @@
+package com.example.springbatch.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class JobConfig {
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+    @Autowired
+    public JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    public StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job ExampleJob(){
+
+        Job exampleJob = jobBuilderFactory.get("exampleJob")
+                .start(startStep())
+                .next(nextStep())
+                .next(lastStep())
+                .build();
+
+        return exampleJob;
+    }
+
+    @Bean
+    public Step startStep() {
+        return stepBuilderFactory.get("startStep")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info("Start Step!");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step nextStep(){
+        return stepBuilderFactory.get("nextStep")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info("Next Step!");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step lastStep(){
+        return stepBuilderFactory.get("lastStep")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info("Last Step!!");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+}
