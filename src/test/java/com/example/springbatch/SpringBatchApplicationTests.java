d0843b77014b7e9d74d1f110d749faf4fcd4ed8c
janghyeonwoo <blue672@naver.com>
2022-03-02 21:27:04 +0900

    젠킨스 test

--- src/test/java/com/example/springbatch/SpringBatchApplicationTests.java
+++ src/test/java/com/example/springbatch/SpringBatchApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.springbatch;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 class SpringBatchApplicationTests {
 
     @Test
     void contextLoads() {
+        List<String> na = new ArrayList<>();
+        na.add("aaaa");
+        System.out.println(na.stream().findAny());
     }
 
 }
